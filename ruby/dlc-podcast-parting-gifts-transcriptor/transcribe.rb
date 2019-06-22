0de562ee97f8c709a8ec4df247c2ab17acb5ec38
pablo.mint <slpkvol3@gmail.com>
2019-06-22 16:08:07 -0300

    Add download method

--- ruby/dlc-podcast-parting-gifts-transcriptor/transcribe.rb
+++ ruby/dlc-podcast-parting-gifts-transcriptor/transcribe.rb
@@ -1,7 +1,16 @@
 require 'json'
 
-def download_episode
+def download_episode url
+  uri = URI(url)
+  file_name = File.basename(uri.path)
 
+  # delete any file first to make sure we get the proper name
+  `rm #{file_name}`
+
+  # download the thing
+  `wget #{url}`
+
+  File.new(file_name)
 end
 
 def cut_section
@@ -23,7 +32,9 @@ def output_text
 
 end
 
-episode_file = download_episode
+url = 'http://director.5by5.tv/d/dlc/cdn.5by5.tv/audio/broadcasts/dlc/2019/dlc-286.mp3'
+
+episode_file = download_episode url
 pg_section_file = cut_section episode_file
 transcription_result = transcribe_section pg_section_file
 raw_text = extract_text transcription_result
