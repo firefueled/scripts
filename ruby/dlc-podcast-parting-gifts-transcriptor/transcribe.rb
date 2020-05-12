1c7075d3eb4990422ce891a84da7f24f7bd581c8
pablo.mint <slpkvol3@gmail.com>
2020-05-12 20:21:40 -0300

    Throw stuff into an output directory

--- ruby/dlc-podcast-parting-gifts-transcriptor/transcribe.rb
+++ ruby/dlc-podcast-parting-gifts-transcriptor/transcribe.rb
@@ -3,20 +3,20 @@ require 'json'
 require 'yaml'
 
 SECRETS = YAML.load(File.read('secrets.yml'))
-base_podcast_url = 'https://director.5by5.tv/d/dlc/5by5.cachefly.net/audio/broadcasts/dlc/2020/'
-ENDPOINT = 'https://stream.watsonplatform.net/speech-to-text/api/v1/recognize'
-@current_file_name = ''
+PODCAST_ENDPOINT = 'https://director.5by5.tv/d/dlc/5by5.cachefly.net/audio/broadcasts/dlc/2020/'
+WATSON_ENDPOINT = 'https://stream.watsonplatform.net/speech-to-text/api/v1/recognize'
+OUTPUT_DIR = 'output'
 
 def download_episode uri
   puts 'downloading episode...'
 
-  @current_file_name = File.basename(uri.path)
+  @current_file_name = File.join(OUTPUT_DIR, File.basename(uri.path))
 
   # delete any file first to make sure we get the proper name
   `rm -f #{@current_file_name}`
 
   # download the thing
-  `wget -q #{uri.to_s}`
+  `wget -q #{uri.to_s} -P #{OUTPUT_DIR}`
   File.new(@current_file_name)
 end
 
@@ -30,10 +30,10 @@ end
 
 def transcribe_section file
   puts 'transcribing section...'
-  text_file_name = File.basename(file) << '.txt'
+  text_file_name = File.join(OUTPUT_DIR, File.basename(file) << '.txt')
   api_key = SECRETS['watson_api_key']
 
-  `curl -X POST -s -u "apikey:#{api_key}" --header "Content-Type: audio/mp3" --data-binary @#{file.path} "#{ENDPOINT}" -o #{text_file_name}`
+  `curl -X POST -s -u "apikey:#{api_key}" --header "Content-Type: audio/mp3" --data-binary @#{file.path} "#{WATSON_ENDPOINT}" -o #{text_file_name}`
   File.new(text_file_name)
 end
 
@@ -47,7 +47,6 @@ def extract_text transcription_file
 end
 
 def differentiate_guests
-
 end
 
 def output_text paragraphs
@@ -74,7 +73,7 @@ if confirmation != 'y' and confirmation != 'Y'
   exit
 end
 
-episode_file = download_episode URI.join(base_podcast_url, 'dlc-' << ep_number << '.mp3')
+episode_file = download_episode URI.join(PODCAST_ENDPOINT, 'dlc-' << ep_number << '.mp3')
 pg_section_file = cut_section episode_file
 transcription_result = transcribe_section pg_section_file
 paragraphs = extract_text transcription_result
